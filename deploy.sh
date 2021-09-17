98149582dcaf2a7870c424242ed9203ca3977e9b
Abin Simon <abinsimon10@gmail.com>
2021-09-17 18:34:03 +0530

    Add personal deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -xe
+
+GOOS=linux GOARCH=amd64 go build
+scp gh-issues-to-rss doom:/tmp/g-temp
+# cannot scp directly to the file when the service is running
+ssh doom "mv /tmp/g-temp /home/ubuntu/gh-issues-to-rss && sudo systemctl restart gh-issues-to-rss"
+rm gh-issues-to-rss
